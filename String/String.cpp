0a8c52ddf63145f3d4bf927953c42cc784d89bc7
hansionz <1776844548@qq.com>
2018-11-11 23:30:31 +0800

    实现string类

---
+++ String/String.cpp
@@ -0,0 +1,139 @@
+#include "String.h"
+
+const size_t zsc::String::npos = -1;
+
+void zsc::String::Reserve(size_t n)
+{
+  if(n > _capacity)
+  {
+    char* newstr = new char[n+1];
+    strcpy(newstr, _str);
+    delete[] _str;
+    _str = newstr;
+    _capacity = n;
+  }
+}
+void zsc::String::Resize(size_t n, char ch)
+{
+  if(n < _size)
+  {
+    _str[n] = '\0';
+  }
+  else 
+  {
+    if(n > _capacity)
+    {
+      Reserve(n);
+    }
+    size_t pos = _size;
+    while(pos < n)
+    {
+      _str[pos] = ch;
+      ++pos;
+    }
+    _str[pos] = '\0';
+    _size = n;
+  }
+}
+void zsc::String::PushBack(char ch)
+{
+  //1.扩容
+  if(_size == _capacity)
+  {
+    Reserve(2*_capacity);
+  }
+  //2.插入元素
+  _str[_size] = ch;
+  _size++;
+}
+void zsc::String::Append(const char* str)
+{
+  size_t len = strlen(str);
+  if((len + _size) > _capacity)
+  {
+    Reserve(_size+len);
+  }
+  strcpy(_str+_size,str);
+  _size += len;
+}
+zsc::String& zsc::String::operator+= (char ch)
+{
+  this->PushBack(ch);
+  return *this;
+}
+zsc::String& zsc::String::operator+=(const char* str)
+{
+  this->Append(str);
+  return *this;
+}
+size_t zsc::String::Find(char ch,size_t pos)
+{
+  assert(pos >= 0);
+  size_t len = _size;
+  size_t i = pos;
+  for(;i < len ;i++)
+  {
+    if(_str[i] == ch)
+    {
+      return i;
+    }
+  }
+  return npos;
+}
+void zsc::String::Insert(char ch, size_t pos)
+{
+  assert(pos >= 0);
+  if((_size+1) > _capacity)
+  {
+    this->Reserve(_size+1);
+  }
+  size_t i = _size;
+  for(;i >= pos;i--)
+  {
+    _str[i+1] = _str[i];
+  }
+  _str[pos] = ch;
+}
+void zsc::String::Insert(const char* str,size_t pos)
+{
+  size_t len = strlen(str);
+  if((_size+len) > _capacity)
+  {
+    this->Reserve(_size+len);
+  }
+  size_t i = pos;
+  for(; i >= pos; i--)
+  {
+    _str[i+len] = _str[i];
+  }
+  for(i = 0;i < len;i++)
+  {
+    _str[i+pos] = str[i];
+  }
+}
+
+void zsc::String::Erase(size_t pos, size_t len)
+{
+  assert(len > 0);
+  size_t i = pos + len;
+  for(; i <= _size; i++ )
+  {
+    _str[i-len] = _str[i];
+  }
+}
+int main()
+{
+  zsc::String s("hello world");
+  cout << s.Find('o',0) << endl;
+  s.Insert('B',1);
+  cout << s.c_str() << endl;
+  s.Erase(2,2);
+  cout << s.c_str() << endl;
+ // cout << s.Capacity() << endl;
+ // s.zsc::String::Reserve(100);
+ // s.Resize(20,'a');
+ // s.PushBack('E');
+ // cout << s.c_str() << endl;
+ // cout << s.Capacity() <<endl;
+  return 0;
+}
---
+++ String/String.h
@@ -0,0 +1,85 @@
+#pragma once 
+
+#include <iostream>
+#include <string.h>
+#include <assert.h>
+using namespace std;
+
+namespace zsc
+{
+  class String
+  {
+  public:
+    String(const char* str = "")
+    {
+      _size = strlen(str);
+      _capacity = _size;
+      _str = new char[_capacity + 1];
+      strcpy(_str, str);
+    }
+    ~String()
+    {
+      delete [] _str;
+      _str = nullptr;
+      _size = _capacity = 0;
+    }
+    void Swap(String& s)
+    {
+      swap(_str, s._str);
+      swap(_capacity, s._capacity);
+      swap(_size, s._size);
+    }
+    String(String const& s)
+      :_str(nullptr)
+      ,_capacity(0)
+      ,_size(0)
+    {
+      String tmp(s._str);
+      this->Swap(tmp);
+    }
+    String& operator=(String s)
+    {
+      Swap(s);
+      return *this;
+    }
+    char& operator[](size_t pos)
+    {
+      assert(pos < _size);
+      return _str[pos];
+    }
+    const char& operator[](size_t pos) const 
+    {
+      assert(pos < _size);
+      return _str[pos];
+    }
+    size_t Size() const 
+    {
+      return _size;
+    }
+    size_t Capacity() const 
+    {
+      return _capacity;
+    }
+    const char* c_str()
+    {
+      return _str;
+    }
+    void Reserve(size_t n);
+    void Resize(size_t n,char ch);
+    void PushBack(char ch);
+    void Append(const char* str);
+    String& operator+=(char ch);
+    String& operator+=(const char* str);
+    size_t Find(char ch,size_t pos = 0);
+    size_t Find(const char* str, size_t pos = 0);
+    void Insert(char ch, size_t pos);
+    void Insert(const char* str,size_t pos);
+    void Erase(size_t pos, size_t len = npos);
+  private:
+    char* _str;
+    size_t _size;
+    size_t _capacity;
+
+    static const size_t npos;
+  };
+}

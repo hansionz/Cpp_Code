6f6a982c7cf7987ef9f3cb4d24aa9039d549917d
HansionZ <1776844548@qq.com>
2018-11-13 22:54:37 +0800

    修改日期类增加重载输入输出

--- DateClass/Main.cpp
+++ DateClass/Main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	Date d1(2018, 10, 29);
+	/*Date d1(2018, 10, 29);
 	d1.Print();
 	d1 += 3;
 	d1.Print();
@@ -13,5 +13,10 @@ int main()
 	Date ol(2018, 10, 27);
 	Date o2(1998, 3, 6);
 	int ret = ol - o2;
-	cout << ret << endl;
+	cout << ret << endl;*/
+	//Date d(2018, 11, 13);
+	//std::cout << d << std::endl;
+	Date d1;
+	cin >> d1;
+	cout << d1 << endl;
 }

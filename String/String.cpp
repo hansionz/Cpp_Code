bc6b89f0dc3e57b40327e4334247457afcfc9caa
hansionz <1776844548@qq.com>
2018-11-19 21:04:18 +0800

    增加String类的一些成员函数的模拟实现

--- String/String.cpp
+++ String/String.cpp
@@ -59,53 +59,102 @@ void zsc::String::Append(const char* str)
   strcpy(_str+_size,str);
   _size += len;
 }
+
 zsc::String& zsc::String::operator+= (char ch)
 {
   this->PushBack(ch);
   return *this;
 }
+
 zsc::String& zsc::String::operator+=(const char* str)
 {
   this->Append(str);
   return *this;
 }
+
 size_t zsc::String::Find(char ch,size_t pos)
 {
   assert(pos >= 0);
-  size_t len = _size;
-  size_t i = pos;
-  for(;i < len ;i++)
+  //size_t len = _size;
+  //size_t i = pos;
+  //for(;i < len ;i++)
+  //{
+  //  if(_str[i] == ch)
+  //  {
+  //    return i;
+  //  }
+  //}
+  while(pos < _size)
   {
-    if(_str[i] == ch)
-    {
-      return i;
-    }
+    if(_str[pos] == ch)
+      return pos;
+    ++pos;
   }
-  return zsc::npos;
+  return zsc::String::npos;
 }
+
+size_t zsc::String::RFind(char ch,size_t pos)
+{ 
+  int end = _size - 1;
+  if(pos != zsc::String::npos)
+  {
+    end = pos;
+  }
+  while(end >= 0)
+  {
+    if(_str[end] == ch)
+    {
+      return end;
+    }
+    --end;
+  }
+  return zsc::String::npos;
+}
+
+zsc::String zsc::String::Substr(size_t pos, size_t len)
+{
+  if(_size - pos < len) //后边的字符不够len个
+  {
+    len = _size - pos;
+  }
+  zsc::String sub; //子串
+  sub.Reserve(len);
+  //strncpy也可以直接拷贝，但是_size等不好改变
+  for(size_t i = pos; i < pos + len; i++)
+  {
+    sub += _str[i];
+  }
+  return sub;
+}
+
 void zsc::String::Insert(char ch, size_t pos)
 {
-  assert(pos >= 0);
+  assert(pos <= _size);
+  //增容
   if((_size+1) > _capacity)
   {
-    this->Reserve(_size+1);
+    Reserve(_capacity * 2);
   }
-  size_t i = _size;
-  for(;i >= pos;i--)
+  int i = _size;
+  for(;i >= (int)pos;i--)
   {
     _str[i+1] = _str[i];
   }
   _str[pos] = ch;
+  _size++;
 }
+
 void zsc::String::Insert(const char* str,size_t pos)
 {
-  size_t len = strlen(str);
+  assert(pos < _size);
+  int len = strlen(str);
+  //扩容
   if((_size+len) > _capacity)
   {
-    this->Reserve(_size+len);
+    Reserve(_size+len);
   }
-  size_t i = pos;
-  for(; i >= pos; i--)
+  int i = _size;
+  for(; i >= (int)pos; i--)
   {
     _str[i+len] = _str[i];
   }
@@ -113,41 +162,70 @@ void zsc::String::Insert(const char* str,size_t pos)
   {
     _str[i+pos] = str[i];
   }
+  _size += len;
 }
 
 void zsc::String::Erase(size_t pos, size_t len)
 {
-  assert(len > 0);
-  size_t i = pos + len +1;
-  for(; i <= _size; i++ )
+  assert(pos < _size);
+  if((len == npos) || (pos + len >= _size))
   {
-    _str[i-len] = _str[i];
+    _str[pos] = '\0';
+    _size = pos;
   }
+  else 
+  {
+    for(size_t i = 0;i < len; i++)
+    {
+      _str[pos] = _str[pos + len];
+      pos++;
+    }
+    _size -= len;
+    _str[_size] = '\0';
+  }
+  //size_t i = pos + len + 1;
+  //for(; i <= _size; i++ )
+  //{
+  //  _str[i-len] = _str[i];
+  //}
+  //_size -= len;
 }
-char* zsc::String::Find(const char* str, size_t pos)
+size_t zsc::String::Find(const char* str, size_t pos)
 {
-  char* parent = _str;
-  const char* sub = str;
-  while(*parent!='\0')
+  assert(pos < _size);
+  char* pmatch = strstr(_str + pos, str);
+  if(pmatch == nullptr)
   {
-    char* cur = parent;
-    const char* start = sub;
-    while(*sub && *cur && (*sub == *cur))
-    {
-      ++sub;
-      ++cur;
-    }
-    if(*sub == '\0')
-    {
-      return (char*)start; 
-    }
-    else 
-    {
-      sub = start;
-    }
-    ++parent;
+    return zsc::String::npos;
   }
-  return NULL;
+  else 
+  {
+    return pmatch - _str;
+  }
+  //自己实现strstr
+  //扩展学习：KMP匹配算法(了解)、后缀树、BM算法
+  //char* parent = _str;
+  //const char* sub = str;
+  //while(*parent!='\0')
+  //{
+  //  char* cur = parent;
+  //  const char* start = sub;
+  //  while(*sub && *cur && (*sub == *cur))
+  //  {
+  //    ++sub;
+  //    ++cur;
+  //  }
+  //  if(*sub == '\0')
+  //  {
+  //    return (char*)start; 
+  //  }
+  //  else 
+  //  {
+  //    sub = start;
+  //  }
+  //  ++parent;
+  //}
+  //return NULL;
 }
 //重载输入
 istream& zsc::operator>>(istream& _cin, String& s)
@@ -161,11 +239,19 @@ ostream& zsc:: operator<<(ostream& _cout,const String& s)
   _cout << s._str;
   return _cout;
 }
-int main()
+void testErase()
 {
   zsc::String s;
-  s = "hello world";
-  cout << s << '\n';
+  s = "helllo";
+  s.Erase(4, 2);
+  cout << s << endl;
+}
+int main()
+{
+  testErase();
+  // zsc::String s;
+  //s = "hello world";
+  //cout << s << '\n';
   //while(cin >> s)
   //  cout << s << endl;
   //cout << s << endl;
--- String/String.h
+++ String/String.h
@@ -13,7 +13,7 @@ namespace zsc
     String(const char* str = "")
     {
       _size = strlen(str);
-      _capacity = _size;
+      _capacity = _size > 15 ? _size : 15;
       _str = new char[_capacity + 1];
       strcpy(_str, str);
     }
@@ -79,7 +79,9 @@ namespace zsc
     String& operator+=(char ch);
     String& operator+=(const char* str);
     size_t Find(char ch,size_t pos = 0);
-    char* Find(const char* str, size_t pos = 0);
+    size_t RFind(char ch,size_t pos = zsc::String::npos);
+    size_t Find(const char* str, size_t pos = 0);
+    String Substr(size_t pos, size_t len);
     void Insert(char ch, size_t pos);
     void Insert(const char* str,size_t pos);
     void Erase(size_t pos, size_t len = npos);
@@ -89,10 +91,10 @@ namespace zsc
     char* _str;
     size_t _size;
     size_t _capacity;
-
+  public:
     static const size_t npos; //-1
   };
-  const size_t npos = -1;
+  const size_t String::npos = -1;
 ostream& operator<<(ostream& _cout,const String& s);
 istream& operator>>(istream& _cin, String& s);
 }

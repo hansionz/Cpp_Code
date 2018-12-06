f60028b6c8d852c566d49a8d2fd5ca69dde170fe
HansionZ <1776844548@qq.com>
2018-12-06 22:54:35 +0800

    增加注释信息

--- Stack/Stack.hpp
+++ Stack/Stack.hpp
@@ -8,26 +8,32 @@ template<class T, class Container = deque<T> >
 class Stack
 {
 public:
+  //入栈
   void Push(const T& data)
   {
     _con.push_back(data);
   }
+  //出栈
   void Pop()
   {
     _con.pop_back();
   }
+  //判断栈是否为空
   bool Empty() const 
   {
     return _con.empty();
   }
+  //栈中元素的个数
   size_t Size() const 
   {
     return _con.size();
   }
+  //获取栈顶元素
   T& Top()
   {
     return _con.back();
   }
+ 
   const T& Top() const 
   {
     return _con.back();
